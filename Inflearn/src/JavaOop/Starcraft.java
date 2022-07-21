76a3e4acf8d637f523a5ca1a4ebab3436c8dadbc
kwonseeun <0115km@naver.com>
2022-07-21 23:47:41 +0900

    marin, medic 클래스 생성

--- Inflearn/src/JavaOop/Starcraft.java
+++ Inflearn/src/JavaOop/Starcraft.java
@@ -2,8 +2,24 @@ package JavaOop;
 
 public class Starcraft {
 	public static void main(String[] args) {
-		//객체 생성
+		// 객체 생성
+		Marine ma = new Marine("레이너", 80);
+		Medic me = new Medic("모랄레스", 60, 60);		
+		
 		// 마린의 스팀팩 
+		
+				
 		// 메딕의 힐 
 	}
+}
+
+class Marine {
+	String name;
+	int hp; 
+}
+
+class Medic {
+	String name;
+	int hp;
+	int mp;
 }
