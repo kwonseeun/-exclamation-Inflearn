956f874b7831cad560e85f73958c588efd8eb22a
kwonseeun <0115km@naver.com>
2022-07-21 23:27:07 +0900

    객체 배열, 객체 파라미터

--- Inflearn/src/JavaOop/SongTest.java
+++ Inflearn/src/JavaOop/SongTest.java
@@ -4,30 +4,30 @@ public class SongTest {
 
 	public static void main(String[] args) {
 
-		//객체 생성 
+		// 객체 생성
 		Song s0 = new Song("별헤는 밤 ", "유재하 ");
 		Song s1 = new Song("비상 ", "임재범 ");
 		Song s2 = new Song("비밀 ", "박완규 ");
-		
-		// 객체배열 생성 
-		Song[] songs =  { s0, s1,s2 };
-		
+
+		// 객체배열 생성
+		Song[] songs = { s0, s1, s2 };
+
 		for (int i = 0; i < songs.length; i++) {
 			System.out.println(songs[i].toStr()); // 각각의 객체를 출력
 		}
-		
 	}
 }
-		class Song {
-			String name;
-			String singer;
-			
-			public Song(String n, String s) {
-				name = n;
-				singer = s;
-			}
-		
-		String toStr() {
-			return String.format("Song { name: %s, singer: %s }", name, singer);
-		}
+
+class Song {
+	String name;
+	String singer;
+
+	public Song(String n, String s) {
+		name = n;
+		singer = s;
+	}
+
+	String toStr() {
+		return String.format("Song { name: %s, singer: %s }", name, singer);
+	}
 }
