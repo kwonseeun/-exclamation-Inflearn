8708af40156f73a49d8ec001e553ea8851a46a97
kwonseeun <0115km@naver.com>
2022-07-25 23:30:29 +0900

    employee 생성자 추가

--- Inflearn/src/JavaOop/Employee.java
+++ Inflearn/src/JavaOop/Employee.java
@@ -3,13 +3,25 @@ package JavaOop;
 public class Employee {
 
 	public static void main(String[] args) {
-		
+	int[] hours0 = {2,4,3,4,5,8,8};
+	
+	Employee e0 = new Employee("직원0", hours0);
+	
+	Employee[] employees = { e0 };
+	
+	for (int i = 0; i < employees.length; i++) {
+		employees[i].printTotalHours();
+		}
 	}
-
 }
 
 class Employee{
 	// 필드 
 	String name;
-	int[] hoyrs;
+	int[] hours;
+	
+	Employee(String str, int[] arr) {
+		name = str;
+		hours = arr;
+	}
 }
