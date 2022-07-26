d6c7711cb6307fe59b6b6d2b0dad086ccc8b32df
kwonseeun <0115km@naver.com>
2022-07-26 23:42:04 +0900

    e1 추가

--- Inflearn/src/JavaOop/EmployeeTest.java
+++ Inflearn/src/JavaOop/EmployeeTest.java
@@ -5,10 +5,14 @@ public class EmployeeTest {
 	public static void main(String[] args) {
 	// 배열 생성 
 	int[] hours0 = {2,4,3,4,5,8,8};
+	int[] hours1 = {7,3,4,3,3,4,4};
 	
 	// 객체 생성 
 	Employee e0 = new Employee("직원0", hours0);
+	Employee e1 = new Employee("직원1", hours1);
 	
+	
+	// 정보 출력 
 	Employee[] employees = { e0 };
 	
 	for (int i = 0; i < employees.length; i++) {
