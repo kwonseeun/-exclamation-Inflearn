0c967f68098186fed1d94e8f59585370b3c9b40a
kwonseeun <0115km@naver.com>
2022-07-26 23:49:51 +0900

    field add

--- Inflearn/src/JavaOop/PlayerTest.java
+++ Inflearn/src/JavaOop/PlayerTest.java
@@ -11,8 +11,21 @@ public class PlayerTest {
 		
 		// 선수 객체 생성 
 		Player p0 = new Player("kim", points0);
+		Player p1 = new Player("Lee", points1);
+		Player p2 = new Player("Park", points2);
 		
 		// 객체 배열 만들기 
 		Player[] play = {p0, p1, p2};
+		
+		for (int i = 0; i < play.length; i++) {
+			Player[i].printTotalPoints();
+		}
 	}
+}
+
+
+class Player {
+	// 필드 생성 
+	String name; 
+	int[] points;
 }
