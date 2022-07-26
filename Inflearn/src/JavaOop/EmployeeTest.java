7d6a5f30780e0ac796eee5d55a854b1705c8a7c9
kwonseeun <0115km@naver.com>
2022-07-26 23:45:20 +0900

    배열, 객체 추가

--- Inflearn/src/JavaOop/EmployeeTest.java
+++ Inflearn/src/JavaOop/EmployeeTest.java
@@ -6,13 +6,25 @@ public class EmployeeTest {
 	// 배열 생성 
 	int[] hours0 = {2,4,3,4,5,8,8};
 	int[] hours1 = {7,3,4,3,3,4,4};
+	int[] hours2 = {3,3,4,3,3,2,2};
+	int[] hours3 = {9,3,4,7,3,4,1};
+	int[] hours4 = {3,5,4,3,3,3,8};
+	int[] hours5 = {3,4,4,6,3,4,4};
+	int[] hours6 = {3,7,4,8,3,8,4};
+	int[] hours7 = {6,3,5,9,3,7,9};
 	
 	// 객체 생성 
 	Employee e0 = new Employee("직원0", hours0);
 	Employee e1 = new Employee("직원1", hours1);
+	Employee e2 = new Employee("직원2", hours2);
+	Employee e3 = new Employee("직원3", hours3);
+	Employee e4 = new Employee("직원4", hours4);
+	Employee e5 = new Employee("직원5", hours5);
+	Employee e6 = new Employee("직원6", hours6);
+	Employee e7 = new Employee("직원7", hours7);
 	
 	
-	// 정보 출력 
+	// 정보 출력 ₩
 	Employee[] employees = { e0, e1 };
 	
 	for (int i = 0; i < employees.length; i++) {
