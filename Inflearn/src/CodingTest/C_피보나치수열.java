a349d9f95812121be81151d876012177e2acce2d
kwonseeun <0115km@naver.com>
2022-09-27 23:29:44 +0900

    update

--- Inflearn/src/CodingTest/C_피보나치수열.java
+++ Inflearn/src/CodingTest/C_피보나치수열.java
@@ -4,5 +4,14 @@ package CodingTest;
  * 다음 숫자가 되는 수열이다. 입력은 피보나치 수열의 총 항의 수 이다.
  * 만약 7이 입력되면 1 1 2 3 5 8 13을 출력하면 된다.*/
 public class C_피보나치수열 {
-
+	public int[] solution(int n) {
+		int[] answer = new int[n];
+		answer[0]=1;
+		answer[1]=1;
+		for (int i = 2; i <n; i++) {
+			answer[i] = answer[i-2]+answer[i-1];
+		}
+		
+		return answer;
+	}
 }
